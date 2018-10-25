1edb1ca01acd48fe7eba15ff805cdd85a1c4627f
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-25 23:29:04 +0700

    Simple Layout and Calculator Layout

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Christiawan Eko Saputro on 01/10/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        view.layoutMargins = UIEdgeInsets(top: 0, left: 64, bottom: 0, right: 0)
+    }
+
+
+}
+
